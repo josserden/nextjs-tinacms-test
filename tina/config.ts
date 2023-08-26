16bc3898fe668a60393aec335cd2d4fc0ef1cb3a
josserden <josserden@gmail.com>
2023-08-26 21:44:43 +0300

    add keys

--- tina/config.ts
+++ tina/config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "tinacms";
 
 export default defineConfig({
   branch: "main",
-  clientId: null,
-  token: null,
+  clientId: "8f396231-0637-4883-a42a-4187214d9f51",
+  token: "519917b71aa8a6503dc4110c495ad7be6a6b800a",
   build: {
     publicFolder: "public",
     outputFolder: "admin",
